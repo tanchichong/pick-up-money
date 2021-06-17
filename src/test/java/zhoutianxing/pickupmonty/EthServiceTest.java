a87d3f571df653584586c01cc17b2cb39acb6aeb
周天星 <zhoutianxing@atgco.cn>
2021-06-17 19:07:32 +0800

    feat:第一版本完成，监控truffle的10个eth地址

--- src/test/java/zhoutianxing/pickupmonty/EthServiceTest.java
+++ src/test/java/zhoutianxing/pickupmonty/EthServiceTest.java
@@ -14,13 +14,13 @@ import java.util.concurrent.ExecutionException;
  */
 class EthServiceTest {
 
-    @Test
+    //@Test
     void getEthBanlance() {
         EthService ethService = EthService.getInstance(1);
         System.out.println(ethService.getEthBanlance("0x627306090abaB3A6e1400e9345bC60c78a8BEf57"));
     }
 
-    @Test
+    //@Test
     void transfer() throws CipherException, IOException, ExecutionException, InterruptedException {
         EthService ethService = EthService.getInstance(1);
         ethService.transfer("0x627306090abaB3A6e1400e9345bC60c78a8BEf57", "0x975795A00554Bb24247da9dAFE9a2d13C3e476Eb", BigInteger.valueOf(10000000000000000L), "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3", BigInteger.valueOf(40000000000L), BigInteger.valueOf(21000L), null);
